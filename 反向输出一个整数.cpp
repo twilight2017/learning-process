d8b4bcdcb07c4fa93ee7554b5115371fdfcba88f
twilight2017 <38035081+twilight2017@users.noreply.github.com>
2019-09-17 19:42:41 +0800

    Update 反向输出一个整数.cpp
    来自LeetCode上的一道简单题目，反向输出输入的整数

--- 反向输出一个整数.cpp
+++ 反向输出一个整数.cpp
@@ -9,7 +9,7 @@ public:
 		int m = 0;//输出值
 		int count = 0;//计数器，用于记录该数字的长度
 		int a = x / pow(10, i);
-		if (x > 0) {
+		
 	
 			while (a != 0) {
 				count++;
@@ -24,22 +24,6 @@ public:
 				count--;
 			}
 			return m;
-		}
-		else {
-			while (a != 0) {
-				count++;
-				i++;
-				a = x / pow(10, i);
-			}
-			while (count > 0) {
-				num = x / pow(10, count - 1);
-				x = x - num * pow(10, count - 1);
-				m += num * pow(10, j);
-				j++;
-				count--;
-			}
-			return m;
-		}
 		}
 	};
     
